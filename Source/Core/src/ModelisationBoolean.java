f4e4a3b4b4097d86bd0c3b92c0d9005d1edcaf73
Launois remy <remy.launois@gmail.com>
2022-06-02 10:00:29 -0400

    Commande code

--- Source/Core/src/ModelisationBoolean.java
+++ Source/Core/src/ModelisationBoolean.java
@@ -1,5 +1,7 @@
 import stev.booleans.*;
 
+import java.util.Arrays;
+
 /**
  * Project: stev-booleans
  * Package: PACKAGE_NAME
@@ -23,9 +25,16 @@ public class ModelisationBoolean {
 
 
         And prop1=getProp1(allNumbersSort);
+//        System.out.println("Proposition 1 : Chaque case ne peut contenir qu’un seul chiffre \n" + prop1);
+
         And prop2 = getProp("prop2",allNumbersSort);
+//        System.out.println("\nProposition 2 : Chaque chiffre doit apparaître exactement une fois dans chaque ligne de la grille \n" + prop2);
+
         And prop3 = getProp("prop3",allNumbersSort);
+//        System.out.println("\nProposition 3 : Chaque chiffre doit apparaître exactement une fois dans chaque colonne de la grille \n" + prop3);
+
         And prop4 = getProp4(allNumbersSort);
+//        System.out.println("\nProposition 4 : Chaque chiffre doit apparaître exactement une fois dans chacune des neuf sous-grilles de taille 3×3 \n" + prop4);
 
         And propTotal = new And(prop1,prop2,prop3,prop4);
 
@@ -33,13 +42,21 @@ public class ModelisationBoolean {
 
     }
 
+    /**
+     * Create the four proposition
+     * @param allNumbersSort
+     * @return
+     */
     private static And getProp4(PropositionalVariable[][][] allNumbersSort){
         And[] allSudoku = new And[3];
         Or[] firstColumnOfSquare = new Or[3];
         Or[] allNumberInCase = new Or[9];
 
+        //Pour chaque Grande colonne (premier indice : 0,0 ; 0,3 ; 0,6)
         for (int c = 0; c < 7; c += 3) {
+            //Pour chaque Grande ligne (premier indice : 0,0 ; 3,0 ; 6,0)
             for (int l = 0; l < 7; l += 3) {
+                //Pour chaque numero
                 for (int n = 0; n < 9; n++) {
                     allNumberInCase[n] = new Or(getNotOfBox(allNumbersSort,l,c,n));
                 }
@@ -74,26 +91,30 @@ public class ModelisationBoolean {
                         }
                     }
                     increment = 0;
-
                     tabImplic[m] = new Implies(allNumbersSort[l][c][m], new Or(allColumnFirstLine));
-
                 }
-
                 tabOrImplicCase[c] = new Or(tabImplic);
             }
-
             tabAndImplic[l] = new And(tabOrImplicCase);
 
         }
-
-
         return new And(tabAndImplic);
     }
 
+    /**
+     * Permet de faire chaque la proposition pour chaque carre de sudoku
+     * @param allNumbersSort
+     * @param l l'indice de ligne de quel case on se situe
+     * @param c l'indice de colonne de quel case on se situe
+     * @param n le nombre qu'on est en train de faire
+     * @return
+     */
     private static Implies[] getNotOfBox(PropositionalVariable[][][] allNumbersSort, int l,int c, int n) {
         Implies[] eachCaseInSquare = new Implies[9];
+        //Pour chaque case du carré
         for (int caseInSquare = 0; caseInSquare < 9; caseInSquare++) {
             PropositionalVariable numberSelected = null;
+            //Permet de choisir le nombre qui sera a gauche de l'implication
             if( caseInSquare < 3){
                 numberSelected = allNumbersSort[l][(c + caseInSquare)][n];
             } else if (caseInSquare < 6) {
@@ -107,6 +128,7 @@ public class ModelisationBoolean {
             Not[] notTheSameNumberInBox = new Not[8];
             PropositionalVariable toKeep;
             int increment = 0;
+            //Pour chaque autre nombre du carre, permet de faire la partie droite de l'implication
             for (int i = 0; i < 10; i++) {
                 if( i < 3 ){
                     toKeep = allNumbersSort[(l)][i+c][n];
